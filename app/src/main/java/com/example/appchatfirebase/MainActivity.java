d0cfe1e84be9d8433fc1062296553e72f16bbd38
Vinicios <viniciuslins61@gmail.com>
2024-05-15 14:26:57 -0300

    C3

--- app/src/main/java/com/example/appchatfirebase/MainActivity.java
+++ app/src/main/java/com/example/appchatfirebase/MainActivity.java
@@ -1,24 +1,68 @@
 package com.example.appchatfirebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.appchatfirebase.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity extends AppCompatActivity {
+ActivityMainBinding binding;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.ButtonLogin.setOnClickListener(click->{
+            Login(binding.EditTextEmail,binding.EditTextPassword);
+        });
+
+        binding.ButtonSigin.setOnClickListener(click->{
+            startActivity(new Intent(this, Signin.class));
+        });
+    }
+
+    void Login(EditText edtEmail, EditText edtSenha)
+    {
+        if (edtEmail.getText().toString().isEmpty() ||edtEmail.getText().toString().isEmpty()) return;
+        mAuth.signInWithEmailAndPassword(edtEmail.getText().toString(),edtSenha.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            startActivity(new Intent(MainActivity.this, AllChats.class));
+                        } else {
+                            Toast.makeText(MainActivity.this, "Um Erro ocorreu:\n"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) startActivity(new Intent(this, AllChats.class));
     }
 }
---
+++ app/src/main/java/com/example/appchatfirebase/Signin.java
@@ -0,0 +1,42 @@
+package com.example.appchatfirebase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.appchatfirebase.databinding.ActivityMainBinding;
+import com.example.appchatfirebase.databinding.ActivitySigninBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Signin extends AppCompatActivity {
+ActivitySigninBinding binding;
+private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivitySigninBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.registerButton.setOnClickListener(click->{
+            String email = binding.emailEditText.getText().toString();
+            String senha = binding.passwordEditText.getText().toString();
+            if (email.isEmpty() || senha.isEmpty()) return;
+            mAuth.createUserWithEmailAndPassword(email,senha)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()){
+                            startActivity(new Intent(Signin.this, MainActivity.class));
+                        } else{
+                            Toast.makeText(this, "Erro ao criar conta:\n"
+                                    + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        });
+    }
+}
