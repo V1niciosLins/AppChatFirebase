469da087efa80d408337bdbce37f452f089284e7
Vinicios <viniciuslins61@gmail.com>
2024-05-15 23:31:18 -0300

    Modificações básicas

--- app/src/main/java/com/example/appchatfirebase/AllChats.java
+++ app/src/main/java/com/example/appchatfirebase/AllChats.java
@@ -8,21 +8,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.appchatfirebase.databinding.ActivityAllChatsBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class AllChats extends AppCompatActivity {
 private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+ActivityAllChatsBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_all_chats);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        binding = ActivityAllChatsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        binding.floating.setOnClickListener(click->{
+            if (binding.floating.isExtended()){
+                binding.floating.shrink();
+            }else {
+                binding.floating.extend();
+            }
+        });
 
     }
 
