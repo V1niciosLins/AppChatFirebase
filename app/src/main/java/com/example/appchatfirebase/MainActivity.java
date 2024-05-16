af91bae88e0fe1bb595792737428bb10bdba8418
Vinicios <viniciuslins61@gmail.com>
2024-05-15 22:40:30 -0300

    Versão mais polida de login e singin

--- app/src/main/java/com/example/appchatfirebase/MainActivity.java
+++ app/src/main/java/com/example/appchatfirebase/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.appchatfirebase;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,9 +18,16 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.appchatfirebase.databinding.ActivityMainBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.FirebaseNetworkException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthEmailException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.internal.api.FirebaseNoSignedInUserException;
 
 public class MainActivity extends AppCompatActivity {
 ActivityMainBinding binding;
@@ -36,6 +46,7 @@ ActivityMainBinding binding;
 
         binding.ButtonLogin.setOnClickListener(click->{
             Login(binding.EditTextEmail,binding.EditTextPassword);
+            binding.LoginProgress.setVisibility(View.VISIBLE);
         });
 
         binding.ButtonSigin.setOnClickListener(click->{
@@ -53,10 +64,32 @@ ActivityMainBinding binding;
                         if (task.isSuccessful()){
                             startActivity(new Intent(MainActivity.this, AllChats.class));
                         } else {
-                            Toast.makeText(MainActivity.this, "Um Erro ocorreu:\n"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            String exception = getException(task);
+
+                            View v = getLayoutInflater().
+                                    inflate(R.layout.alert,null,false);
+                            ((TextView) v.findViewById(R.id.AlertText)).setText(exception);
+                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                            builder.setView(v);
+                            builder.setCancelable(true);
+                            AlertDialog alert = builder.create();
+                            alert.show();
+                            binding.LoginProgress.setVisibility(View.GONE);
                         }
                     }
                 });
+    }
+
+    @NonNull
+    private static String getException(Task<AuthResult> task) {
+        String exception ="Erro desconhecido.\nTente novamente.";
+        if (task.getException() instanceof FirebaseAuthInvalidUserException)
+            exception="Usuário não existe ou está desativado.";
+        if (task.getException() instanceof FirebaseAuthEmailException)
+            exception="Email fornecido é inválido.";
+        if (task.getException() instanceof FirebaseNetworkException)
+            exception="Erro de internet";
+        return exception;
     }
     @Override
     public void onStart() {
--- app/src/main/java/com/example/appchatfirebase/Signin.java
+++ app/src/main/java/com/example/appchatfirebase/Signin.java
@@ -2,9 +2,14 @@ package com.example.appchatfirebase;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -12,7 +17,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.appchatfirebase.databinding.ActivityMainBinding;
 import com.example.appchatfirebase.databinding.ActivitySigninBinding;
+import com.example.appchatfirebase.databinding.AlertBinding;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseNetworkException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthEmailException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class Signin extends AppCompatActivity {
 ActivitySigninBinding binding;
@@ -25,20 +38,42 @@ private FirebaseAuth mAuth = FirebaseAuth.getInstance();
         setContentView(binding.getRoot());
 
         binding.registerButton.setOnClickListener(click->{
-            String email = binding.emailEditText.getText().toString();
+            String email = binding.emailEditText.getText().toString().trim();
             String senha = binding.passwordEditText.getText().toString();
             if (email.isEmpty() || senha.isEmpty()) return;
-
+            binding.Progress.setVisibility(View.VISIBLE);
             mAuth.createUserWithEmailAndPassword(email,senha)
                     .addOnCompleteListener(task -> {
                         if (task.isSuccessful()){
                             startActivity(new Intent(Signin.this, MainActivity.class));
                         } else{
-                            Toast.makeText(this, "Erro ao criar conta:\n"
-                                    + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            String exception = getException(task);
+
+                            View v = getLayoutInflater().
+                                    inflate(R.layout.alert,null,false);
+                            ((TextView) v.findViewById(R.id.AlertText)).setText(exception);
+                            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                            builder.setView(v);
+                            builder.setCancelable(true);
+                            AlertDialog alert = builder.create();
+                            alert.show();
+                            binding.Progress.setVisibility(View.GONE);
+
                         }
                     });
 
         });
+    }
+
+    @NonNull
+    private static String getException(Task<AuthResult> task) {
+        String exception ="Erro desconhecido.\nTente novamente";
+        if (task.getException() instanceof FirebaseAuthWeakPasswordException)
+            exception="Senha fraca, precisa conter 6 caracteres ou mais.";
+        if (task.getException() instanceof FirebaseAuthUserCollisionException)
+            exception="Já existe um usuário registrado com esta conta.";
+        if (task.getException() instanceof FirebaseNetworkException)
+            exception="Erro de internet";
+        return exception;
     }
 }
